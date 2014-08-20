a7a472742bf0e6fbceea922bd08ed536ef75e09e
Stanley Gu <stanleygu@gmail.com>
2014-08-20 06:41:35 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+version = '0.0.4'
+
+setup(name='notebooktools',
+      version=version,
+      description='Tools for IPython Notebook used with Tellurium',
+      author='Stanley Gu',
+      author_email='stanleygu@gmail.com',
+      url='https://github.com/stanleygu/ipython-notebook-modules'
+      )
