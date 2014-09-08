bf94fb9722d32eaca66888994b2bd9cb6c4fc511
Stanley Gu <stanleygu@gmail.com>
2014-09-08 17:12:15 +0000

    Actually include diffevolution package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from distutils.core import setup
 
-version = '0.0.6'
+version = '0.0.7'
 
 setup(name='notebooktools',
       version=version,
@@ -10,5 +10,5 @@ setup(name='notebooktools',
       author='Stanley Gu',
       author_email='stanleygu@gmail.com',
       url='https://github.com/stanleygu/ipython-notebook-modules',
-      packages=['notebooktools']
+      packages=['notebooktools', 'diffevolution']
       )
